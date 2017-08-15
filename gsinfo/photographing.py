5be22c051f6046c002bf2585c7436892b5f401d0
wangzeya <wzy@metadata.net.cn>
2017-08-15 10:16:41 +0800

    图像采集bug

--- gsinfo/photographing.py
+++ gsinfo/photographing.py
@@ -52,54 +52,57 @@ def updatePhotographingInfo(request):
     serialNumber = request.GET.get('serialNumber', '')
     boxOrSubBox = request.GET.get('boxNumber', '')
     pic_path = request.GET.get('pic_path', '')
-
-    img_path = json.loads(pic_path)
-    for k,v in img_path.items():
-        file_name = serialNumber + '-' + k + '.jpg'
-        save_path = os.path.join(settings.STATIC_PATH,'img',boxOrSubBox,serialNumber,file_name)
-        img = base64.b64decode(v)
-        with open(save_path,'wb') as f:
-            f.write(img)
-
     ret = {}
-    if '-' in boxOrSubBox:
-        boxNumber = int(boxOrSubBox.split('-')[0])
-        subBoxNumber = int(boxOrSubBox.split('-')[1])
+    if pic_path:
+        img_path = json.loads(pic_path)
+
+        for k,v in img_path.items():
+            file_name = serialNumber + '-' + k + '.jpg'
+            save_path = os.path.join(settings.STATIC_PATH,'img',boxOrSubBox,serialNumber,file_name)
+            img = base64.b64decode(v)
+            with open(save_path,'wb') as f:
+                f.write(img)
+
+        if '-' in boxOrSubBox:
+            boxNumber = int(boxOrSubBox.split('-')[0])
+            subBoxNumber = int(boxOrSubBox.split('-')[1])
+        else:
+            boxNumber = int(boxOrSubBox)
+            subBoxNumber = ''
+
+
+        user = request.user
+        operator = gsUser.objects.get(user=user).nickName
+
+        box = gsBox.objects.get(boxNumber=boxNumber)
+
+        try:
+            # 检测作业是否可用
+            t = gsThing.objects.get(serialNumber=serialNumber)
+            wt = gsWorkThing.objects.get(thing=t)
+            if wt.work.status != 1:
+                # 作业不可用
+                raise ValueError, u'作业不可用！请联系现场负责人进行分发，并刷新页面！'
+
+            # now = datetime.datetime.utcnow()  # 这里使用utcnow生成时间,存入mariaDB后被数据库当做非UTC时间,自动减去了8个小时,所以这里改用now
+            now = datetime.datetime.now()
+            gsStatus.objects.filter(box=box, serialNumber=serialNumber).update(photographingStatus=True,
+                                                                               photographingOperator=operator,
+                                                                               photographingUpdateDateTime=now)
+
+            s = gsStatus.objects.get(box=box, serialNumber=serialNumber)
+            status = s.numberingStatus and s.analyzingStatus and s.measuringStatus and s.photographingStatus
+            gsStatus.objects.filter(box=box, serialNumber=serialNumber).update(status=status)
+        except Exception as e:
+            ret['success'] = False
+            ret['message'] = '图片上传失败！'
+        else:
+            ret['success'] = True
+            ret['message'] = '图片上传成功！'
     else:
-        boxNumber = int(boxOrSubBox)
-        subBoxNumber = ''
-
-
-    user = request.user
-    operator = gsUser.objects.get(user=user).nickName
-
-    box = gsBox.objects.get(boxNumber=boxNumber)
-
-    try:
-        # 检测作业是否可用
-        t = gsThing.objects.get(serialNumber=serialNumber)
-        wt = gsWorkThing.objects.get(thing=t)
-        if wt.work.status != 1:
-            # 作业不可用
-            raise ValueError, u'作业不可用！请联系现场负责人进行分发，并刷新页面！'
-
-        # now = datetime.datetime.utcnow()  # 这里使用utcnow生成时间,存入mariaDB后被数据库当做非UTC时间,自动减去了8个小时,所以这里改用now
-        now = datetime.datetime.now()
-        gsStatus.objects.filter(box=box, serialNumber=serialNumber).update(photographingStatus=True,
-                                                                           photographingOperator=operator,
-                                                                           photographingUpdateDateTime=now)
-
-        s = gsStatus.objects.get(box=box, serialNumber=serialNumber)
-        status = s.numberingStatus and s.analyzingStatus and s.measuringStatus and s.photographingStatus
-        gsStatus.objects.filter(box=box, serialNumber=serialNumber).update(status=status)
-    except Exception as e:
         ret['success'] = False
         ret['message'] = '图片上传失败！'
-    else:
-        ret['success'] = True
-        ret['message'] = '图片上传成功！'
 
     ret_json = json.dumps(ret, separators=(',', ':'))
-
     return HttpResponse('success_jsonpCallback(' + ret_json + ')')
 
