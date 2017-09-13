b2d81c6ee290b2d56b18a59831a0f56dd9075e01
wangzeya <wzy@metadata.net.cn>
2017-09-13 09:44:37 +0800

    装盒票

--- gsinfosite/gsinfo/manage.py
+++ gsinfosite/gsinfo/manage.py
@@ -15,7 +15,9 @@ from . import log
 import MySQLdb
 import win32print
 import win32api
-import requests
+import win32ui
+# import requests
+from PIL import Image, ImageWin
 import shortuuid
 from gsinfosite import settings
 
@@ -1623,6 +1625,95 @@ def print_service(request):
     return HttpResponse(ret_json)
 
 
+def print_pic(request):
+    # "Standard" win32 printer capability values:
+    #    HORZSIZE	        : width of full page in mm
+    #    VERTSIZE		    : height of full page in mm
+    #    HORZRES            : device units in width of printable area
+    #    VERTRES            : device units in height of printable area
+    #    PHYSICALWIDTH      : device units in full page width
+    #    PHYSICALHEIGHT     : device units in full page height
+    #    PHYSICALOFFSETX    : left device margin of unprintable area
+    #    PHYSICALOFFSETY    : top device margin of unprintable area
+    #    LOGPIXELSX         : device units per inch of horoz. axis (usually the DPI of the printer)
+    #    LOGPIXELSY         : device units per inch of vert. axis (usually the DPI of the printer)
+    try:
+        # HORZRES / VERTRES = printable area
+        HORZRES = 8
+        VERTRES = 10
+        # LOGPIXELS = dots per inch
+        LOGPIXELSX = 88
+        LOGPIXELSY = 90
+        # PHYSICALWIDTH/HEIGHT = total area
+        PHYSICALWIDTH = 110
+        PHYSICALHEIGHT = 111
+        # PHYSICALOFFSETX/Y = left / top margin
+        PHYSICALOFFSETX = 112
+        PHYSICALOFFSETY = 113
+
+        printer_name = win32print.GetDefaultPrinter()
+        file_name = r"G:/items/BullionCheckSys/gsinfosite/data/tag/serialNumber2/1/wufjTN4o2X.png"
+
+        # You can only write a Device-independent bitmap
+        #  directly to a Windows device context; therefore
+        #  we need (for ease) to use the Python Imaging
+        #  Library to manipulate the image.
+        #
+        # Create a device context from a named printer
+        #  and assess the printable size of the paper.
+
+        hDC = win32ui.CreateDC()
+        hDC.CreatePrinterDC(printer_name)
+        printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
+        printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
+        printer_margins = hDC.GetDeviceCaps(PHYSICALOFFSETX), hDC.GetDeviceCaps(PHYSICALOFFSETY)
+
+        #
+        # Open the image, rotate it if it's wider than
+        #  it is high, and work out how much to multiply
+        #  each pixel by to get it as big as possible on
+        #  the page without distorting.
+        #
+        bmp = Image.open(file_name,mode="r")
+        if bmp.size[0] > bmp.size[1]:
+            bmp = bmp.rotate(90)
+
+        # ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
+        # scale = min(ratios)
+
+        #
+        # Start the print job, and draw the bitmap to
+        #  the printer device at the scaled size.
+        #
+        hDC.StartDoc(file_name)
+        hDC.StartPage()
+
+        dib = ImageWin.Dib(bmp)
+        scaled_width, scaled_height = [int(3*i) for i in bmp.size]  # 对原始图片放大n倍
+        x1 = int((printer_size[0] - scaled_width) / 4)
+        y1 = int((printer_size[1] - scaled_height) / 4)
+        x2 = x1 + scaled_width
+        y2 = y1 + scaled_height
+        dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
+
+        hDC.EndPage()
+        hDC.EndDoc()
+        hDC.DeleteDC()
+    except Exception as e:
+        ret = {
+            'success': False,
+            'message': u'打印失败！'
+        }
+    else:
+        ret = {
+            'success': True,
+            'message': u'打印成功！'
+        }
+    ret_json = json.dumps(ret, separators=(',', ':'))
+    return HttpResponse(ret_json)
+
+
+
 def print_auth(request):
     nickName = request.POST.get('user', '')  # 系统负责人用户名
     password = request.POST.get('password', '')  # 系统负责人密码
@@ -1787,9 +1878,9 @@ def getCaseNumber(request):
     return HttpResponse(ret_json)
 
 def confirmInputCase(request):
-    boxNumber = request.POST.get('boxNumber', '')
-    caseNumber = request.POST.get('caseNumber', '')
-    serialNumber2= request.POST.get('serialNumber2', '')
+    boxNumber = 1 # request.POST.get('boxNumber', '')
+    caseNumber = '456Wuyh2' # request.POST.get('caseNumber', '')
+    serialNumber2= '123bJw3D;123SnGu2;123DPKek;' # request.POST.get('serialNumber2', '')
     serialNumber2_list = serialNumber2.split(';')[0:-1]
     try:
         case = gsCase.objects.create(caseNumber=caseNumber,status=0)
@@ -1821,9 +1912,6 @@ def text(request):
     a = gsThing._meta.get_fields()
     vv = ee.id in a
     ww =ee.tt in a
-    b = gsThing._meta.get_fields(include_parents = True, include_hidden = True)
-    c = gsThing._meta.get_fields(include_parents=False, include_hidden=False)
-    d = gsThing._meta.get_fields(include_parents=False, include_hidden=True)
     # th = gsThing.objects.filter(serialNumber='F4hJrUswL5').__getattribute__()
     # dd = th.getattr('aa','')
     # ss = th.aa.exists()
