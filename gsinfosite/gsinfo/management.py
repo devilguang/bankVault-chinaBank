335c8f554c7c14a0e0ce091239cdb3ef9a0908a5
wangzeya <wzy@metadata.net.cn>
2017-09-16 10:59:18 +0800

    修改表

--- gsinfosite/gsinfo/management.py
+++ gsinfosite/gsinfo/management.py
@@ -8,118 +8,47 @@ def batchInsert(sender, **kwargs):
         init_property()
         init_authority()
 
-
 def init_property():
-    add_property(project='实物类型', type='金银锭类', code='1')
-    add_property(project='品名', type='杂金', code='1', parentProject='实物类型', parentType='金银锭类')
-    add_property(project='品名', type='杂银', code='2', parentProject='实物类型', parentType='金银锭类')
-
-    add_property(project='明细品名', type='稀一锭', code='02', parentProject='品名', parentType='杂金', grandpaProject='实物类型',grandpaType='金银锭类')
-    add_property(project='明细品名', type='稀三锭', code='08', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银锭类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银锭类')
-
-    add_property(project='明细品名', type='稀一锭', code='02', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银锭类')
-    add_property(project='明细品名', type='稀二锭', code='06', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银锭类')
-    add_property(project='明细品名', type='稀三锭', code='10', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银锭类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银锭类')
-    # ------------------------------------------------------------------------------------------------------------------
-    add_property(project='实物类型', type='金银币章类', code='2')
-    add_property(project='品名', type='杂金', code='1', parentProject='实物类型', parentType='金银币章类')
-    add_property(project='品名', type='杂银', code='2', parentProject='实物类型', parentType='金银币章类')
-
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银锭类'
-                 )
-    add_property(project='明细品名', type='稀一币', code='01', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='稀二币', code='04', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='稀三币', code='06', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='金样币', code='10', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-
-    add_property(project='明细品名', type='稀一元', code='01', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='稀二外元', code='03', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='稀二减元', code='04', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='稀二钱', code='05', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='稀三色元', code='09', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='稀三辅币', code='08', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='银样币', code='14', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银币章类')
-    # ------------------------------------------------------------------------------------------------------------------
-    add_property(project='实物类型', type='银元类', code='3')
-    add_property(project='品名', type='国内稀一级银元', code='3', parentProject='实物类型', parentType='银元类')
-    add_property(project='品名', type='国内稀二级银元', code='4', parentProject='实物类型', parentType='银元类')
-    add_property(project='品名', type='国内稀三级银元', code='5', parentProject='实物类型', parentType='银元类')
-    add_property(project='品名', type='国外稀二级银元', code='6', parentProject='实物类型', parentType='银元类')
-    add_property(project='品名', type='普制银元', code='7', parentProject='实物类型', parentType='银元类')
-    add_property(project='品名', type='待熔银元8', code='8', parentProject='实物类型', parentType='银元类')
-
-    add_property(project='明细品名', type='待熔银元', code='01', parentProject='品名', parentType='待熔银元8', grandpaProject='实物类型',
-                 grandpaType='银元类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='待熔银元8', grandpaProject='实物类型',
-                 grandpaType='银元类')
-    add_property(project='明细品名', type='袁像民国三年壹圆', code='01', parentProject='品名', parentType='普制银元',grandpaProject='实物类型',
-                 grandpaType='银元类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='普制银元', grandpaProject='实物类型',
-                 grandpaType='银元类')
-    add_property(project='明细品名', type='造币总厂光绪元宝库平七钱二分', code='01', parentProject='品名', parentType='国内稀三级银元',
-                 grandpaProject='实物类型', grandpaType='银元类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='国内稀三级银元', grandpaProject='实物类型',
-                 grandpaType='银元类')
-    add_property(project='明细品名', type='西班牙币', code='01', parentProject='品名', parentType='国外稀二级银元',grandpaProject='实物类型',
-                 grandpaType='银元类')
-    add_property(project='明细品名', type='丁未大清银币壹圆', code='01', parentProject='品名', parentType='国内稀二级银元',
-                 grandpaProject='实物类型', grandpaType='银元类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='国内稀二级银元', grandpaProject='实物类型',
-                 grandpaType='银元类')
-    add_property(project='明细品名', type='福建官局造光绪元宝', code='01', parentProject='品名', parentType='国内稀一级银元',
-                 grandpaProject='实物类型', grandpaType='银元类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='国内稀一级银元', grandpaProject='实物类型',
-                 grandpaType='银元类')
-    # ------------------------------------------------------------------------------------------------------------------
-    add_property(project='实物类型', type='金银工艺品类', code='4')
-    add_property(project='品名', type='杂金', code='1', parentProject='实物类型', parentType='金银工艺品类')
-    add_property(project='品名', type='杂银', code='2', parentProject='实物类型', parentType='金银工艺品类')
-
-    add_property(project='明细品名', type='稀一工', code='03', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银工艺品类')
-    add_property(project='明细品名', type='稀二工', code='05', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银工艺品类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='杂金', grandpaProject='实物类型',
-                 grandpaType='金银工艺品类')
-    add_property(project='明细品名', type='稀二工', code='07', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银工艺品类')
-    add_property(project='明细品名', type='稀三工', code='12', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银工艺品类')
-    add_property(project='明细品名', type='普制工', code='13', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银工艺品类')
-    add_property(project='明细品名', type='混装', code='00', parentProject='品名', parentType='杂银', grandpaProject='实物类型',
-                 grandpaType='金银工艺品类')
-
     for dic in metaDate:
         project = dic['project']
         val = dic['val']
         for type_code in val:
             code,type = type_code
             add_property(project=project, type=type, code=code)
+
+    for dic1 in detail:
+        grandpaProject = dic1['grandpaProject']
+        grandpaType = dic1['grandpaType']
+        grandpaCode = dic1['grandpaCode']
+        val1 = dic1['val']
+        for dic2 in val1:
+            parentProject = dic2['parentProject']
+            parentType = dic2['parentType']
+            parentCode = dic2['parentCode']
+            val2 = dic2.get('val','')
+            if val2:
+                for dic3 in val2:
+                    project = dic3['parentProject']
+                    type = dic3['parentType']
+                    code = dic3['parentCode']
+                    add_property(project=project,
+                                 type=type,
+                                 code=code,
+                                 parentProject=parentProject,
+                                 parentType=parentType,
+                                 parentCode=parentCode,
+                                 grandpaProject=grandpaProject,
+                                 grandpaType=grandpaType,
+                                 grandpaCode=grandpaCode,
+                                 )
+
+            else:
+                add_property(project=parentProject,
+                             type=parentType,
+                             code=parentCode,
+                             parentProject=grandpaProject,
+                             parentType=grandpaType,
+                             parentCode=grandpaCode,)
 
 def add_property(project, type, code=None, parentProject='', parentType='', grandpaProject='', grandpaType=''):
     from gsinfo.models import gsProperty
